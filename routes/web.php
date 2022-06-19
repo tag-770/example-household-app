ab6aadfec4e6d553fbcfd2c64e5480a94bb48a76
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-06-19 17:12:02 +0900

    会員登録ページを表示するためにroutesを編集した

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,4 @@ Route::get('/', function () {
 });
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
