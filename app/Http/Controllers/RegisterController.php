47b9a08bb8465bc949d2bacde53f484482eadc42
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-06-19 17:14:16 +0900

    homeにリダイレクトする処理を書いた

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -10,4 +10,9 @@ class RegisterController extends Controller
     {
         return view('register');
     }
+
+    public function store()
+    {
+        return redirect()->route('home');
+    }
 }
