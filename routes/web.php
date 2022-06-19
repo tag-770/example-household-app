ca1bd9354e619e0d8eccce86a6560ab1c32bcac3
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-06-19 18:01:59 +0900

    ログアウト用のroutesを追加した

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,4 @@ Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
