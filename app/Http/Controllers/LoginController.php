b90e68b200fefb1b2abe83560c7ccc96b24a1178
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-06-19 17:51:36 +0900

    ログインフォームのバリデーションを追加した

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
@@ -13,6 +14,10 @@ class LoginController extends Controller
 
     public function login(Request $request)
     {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
         return redirect()->route('home');
     }
 }
