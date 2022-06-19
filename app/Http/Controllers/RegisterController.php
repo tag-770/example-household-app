1a855675292123212e8d514cb579818ae6ec5b2f
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-06-19 17:21:02 +0900

    Userを追加する処理をコントローラに追加した

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -11,8 +13,13 @@ class RegisterController extends Controller
         return view('register');
     }
 
-    public function store()
+    public function store(Request $request)
     {
+        $user = User::create([
+            'name' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
         return redirect()->route('home');
     }
 }
