211488fb01817f9153b2f10f64c3ee79f9daa416
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-06-19 17:47:30 +0900

    ログイン用のメソッドを追加した

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,4 +10,9 @@ class LoginController extends Controller
     {
         return view('login');
     }
+
+    public function login(Request $request)
+    {
+        return redirect()->route('home');
+    }
 }
