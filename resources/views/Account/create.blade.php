0dc229d65b73e4ba0c03e3a95f26bf8b4cdf9046
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-06-20 22:56:09 +0900

    家計簿を登録するbladeを作成した

---
+++ resources/views/Account/create.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>家計簿作成 | (参考用)家計簿アプリ</title>
+</head>
+<body>
+    <header></header>
+    <main>
+        <form action="" method="post">
+            <label for="title">タイトル</label>
+            <input type="text" name="title" id="title">
+            <label for="amount">金額</label>
+            <input type="number" name="amount" id="amount">
+            <label for="memo">メモ</label>
+            <textarea name="memo" id="memo" cols="30" rows="10"></textarea>
+            <button type="submit">送信する</button>
+        </form>
+    </main>
+    <footer></footer>
+</body>
+</html>
